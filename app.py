0646ef5d718c977a80000eb221e053f7134b0f96
davidprager <84202740+davidprager@users.noreply.github.com>
2024-12-27 10:12:12 -0500

    Initial Commit

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 # instantiate the app
 app = Flask(__name__)
 
-# Create a dictionary called categories, the keys should be the category ids. The values should be the categories.
+# Create a list called categories. The elements in the list should be lists that contain the following information in this order:
+#   categoryId
+#   categoryName
+#   An example of a single category list is: [1, "Biographies"]
 
 # Create a list called books. The elements in the list should be lists that contain the following information in this order:
-#   bookId     (you can assign the bookId - preferably a number from 1 -16)
+#   bookId     (you can assign the bookId - preferably a number from 1-16)
 #   categoryId (this should be one of the categories in the category dictionary)
 #   title
 #   author
@@ -15,6 +18,7 @@ app = Flask(__name__)
 #   price      (the value should be a float)
 #   image      (this is the filename of the book image.  If all the images, have the same extension, you can omit the extension)
 #   readNow    (This should be either 1 or 0.  For each category, some of the books (but not all) should have this set to 1.
+#   An example of a single category list is: [1, 1, "Madonna", "Andrew Morton", "13-9780312287863", 39.99, "madonna.png", 1]
 
 
 
